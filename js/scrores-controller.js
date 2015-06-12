db2eedf13be521ce6daac69cc69a31d8fef1ccb5
Kevin Seiter <kevin.seiter@galavantier.com>
2015-06-12 11:10:44 -0700

    updates to scores controller.

--- js/scrores-controller.js
+++ js/scrores-controller.js
@@ -1,6 +1,6 @@
-var statsController = function() {
+var scoresController = function() {
   var controller = {
-    stats: [],
+    scores: [],
     maxStatsCount: 20,
 
     getStats: function() {
@@ -15,18 +15,18 @@ var statsController = function() {
 
     addStat: function(initials, score) {
       var self = this;
-      // Add user initials and score to stats
-      self.stats.push({initials: initials, score: score});
+      // Add user initials and score to scores array
+      self.scores.push({initials: initials, score: score});
       self.sortScoreboard();
     },
 
     isScoreboardWorthy: function(score) {
       var self = this;
 
-      if (self.stats.length < self.maxStatsCount) { return true; }
+      if (self.scores.length < self.maxStatsCount) { return true; }
 
-      for (var i = 0; i < self.stats.length; i++) {
-        if (self.stats[i].score < score) { return true; }
+      for (var i = 0; i < self.scores.length; i++) {
+        if (self.scores[i].score < score) { return true; }
       }
 
       return false;
@@ -35,19 +35,19 @@ var statsController = function() {
     sortScoreboard: function() {
       var self = this;
 
-      self.stats.sort(function(statA, statB) {
-        if (statA.score < statB.score) {
+      self.scores.sort(function(scoreA, scoreB) {
+        if (scoreA.score < scoreB.score) {
           return -1;
         }
-        if (statA.score > statB.score) {
+        if (scoreA.score > scoreB.score) {
           return 1;
         }
 
         // If score is same, alphabetize.
-        if (statA.initials < statB.initials) {
+        if (scoreA.initials < scoreB.initials) {
           return -1;
         }
-        else if (statA.initials > statB.initials) {
+        else if (scoreA.initials > scoreB.initials) {
           return 1;
         }
 
